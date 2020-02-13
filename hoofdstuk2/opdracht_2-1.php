063c60a51ffef1b923c11d0658d176717ca777ab
Mark Sloesen <marksloesen.email@edu-kw1c.nl>
2020-02-13 09:18:55 +0100

    template en opdracht 2.1

--- hoofdstuk2/opdracht_2-1.php
+++ hoofdstuk2/opdracht_2-1.php
@@ -14,16 +14,62 @@
 <header>
     <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
 </header>
+<div id="content">
 <aside>
     <h2>Menu</h2>
     <ul>
         <li>Hoofdstuk 2
             <ul>
                 <li>
-                    <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                    <a href="opdracht_2-1.php">Opdracht 2.1</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                    <a href="opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 3
+            <ul>
+                <li>
+                    <a href="hoofdstuk3/opdracht_3-1.php">Opdracht 3.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk3/opdracht_3-2.php">Opdracht 3.2</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk3/opdracht_3-3.php">Opdracht 3.3</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 4
+            <ul>
+                <li>
+                    <a href="hoofdstuk4/opdracht_4-1.php">Opdracht 4.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk4/opdracht_4-2.php">Opdracht 4.2</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk4/opdracht_4-3.php">Opdracht 4.3</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk4/opdracht_4-4.php">Opdracht 4.4</a>
+                </li>
+            </ul>
+        </li>
+        <li>Hoofdstuk 5
+            <ul>
+                <li>
+                    <a href="hoofdstuk5/opdracht_5-1.php">Opdracht 5.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk5/opdracht_5-2.php">Opdracht 5.2</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk5/opdracht_5-3.php">Opdracht 5.3</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk5/opdracht_5-4.php">Opdracht 5.4</a>
                 </li>
             </ul>
         </li>
@@ -33,6 +79,7 @@
     <h2>Uitwerkingen</h2>
     <p><?php echo "Hello World!"?></p>
 </main>
+</div>
 </body>
 </html>
 
